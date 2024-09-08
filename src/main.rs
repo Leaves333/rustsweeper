65d551d8f062d489cd0e4390c7f56ead35d11b18
Leaves333 <canchenli@gmail.com>
2024-09-08 16:22:18 -0500

    testing out console

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use console::*;
+
 fn main() {
-    println!("Hello, world!");
+    let term = Term::stdout();
+    let x = 490;
+    let red = Style::new().red();
+    let text = format!("x is {}", red.apply_to(x));
+    let _ = term.write_line(&text);
 }
